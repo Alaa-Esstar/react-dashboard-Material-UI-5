7fa974d1bf2b633fb4075c0985224fb72e771c9c
Alaa Esstar <alaa.esstar@gmail.com>
2023-11-09 21:12:16 +0100

    first commit

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+// import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+// import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+// import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+// import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+// import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+// import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+// import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+// import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+// import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+// import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+// import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import 'react-pro-sidebar/dist/css/styles.css';
+
+const Sidebar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [selected, setSelected] = useState("Dashboard");
+    return (
+        <Box
+            sx={{
+                "& .pro-sidebar-inner": {
+                    background: `${colors.primary[400]} !important`
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important"
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important"
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important"
+                },
+                "& .pro-menu-item.active": {
+                    color: "#6870fa !important"
+                }
+            }}
+        >
+            <ProSidebar collapsed={isCollapsed}>
+                <Menu iconShape="square">
+                    <MenuItem onClick={() => setIsCollapsed(!isCollapsed)}
+                        icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+                        style={{
+                            margin: "10px 0 10px 0",
+                            color: colors.gray[100],
+                        }}>
+                        {!isCollapsed && (
+                            <Box
+                                display="flex"
+                                justifyContent="space-between"
+                                alignItems="center"
+                                ml="15px"
+                            >
+                                <Typography variant="h3" color={colors.gray[100]}>
+                                    Admins
+                                </Typography>
+                                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                                    <MenuOutlinedIcon />
+                                </IconButton>
+                            </Box>
+                        )}
+                    </MenuItem>
+                    {/* User */}
+                    {!isCollapsed && (
+                        <Box mb="25px">
+                            <Box display="flex" justifyContent="center" alignItems="center">
+                                <img
+                                    alt="profile-user"
+                                    width="100px"
+                                    height="100px"
+                                    src={`../../assets/user.png`}
+                                    style={{ cursor: "pointer", borderRadius: "50%" }} />
+                            </Box>
+                            <Box textAlign="center">
+                                <Typography
+                                    variant="h2"
+                                    color={colors.gray[100]}
+                                    fontWeight="bold"
+                                    sx={{
+                                        m: "10px 0 0 0"
+                                    }}>
+                                    Ed Roh
+                                </Typography>
+                                <Typography variant="h5" color={colors.greenAccent[500]}>VP Fancy Admin</Typography>
+                            </Box>
+                        </Box>
+                    )}
+                </Menu>
+            </ProSidebar>
+        </Box>
+    )
+}
+
+export default Sidebar;
